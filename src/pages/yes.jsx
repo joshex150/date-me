535be47d489be47f973f2d268245409a4d9a3e97
joshex150 <joshex150@gmail.com>
2023-12-23 22:45:35 +0100

    new

--- src/pages/yes.jsx
+++ src/pages/yes.jsx
@@ -12,7 +12,7 @@ const yes = () => {
           justify-content: center;
           align-items: center;
           height: 100vh;
-          background-color: #c095e6;
+          background-color: #000;
           min-width: 100vw;
           overflow: hidden;
         }
